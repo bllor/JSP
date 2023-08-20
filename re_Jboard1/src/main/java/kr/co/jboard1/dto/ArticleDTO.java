75f06c6cb1f0698e351350e0b9ddab635324189e
bllor <duki0105@gmail.com>
2023-08-20 21:36:08 +0900

    DAO, DB, DTO 추가

---
+++ re_Jboard1/src/main/java/kr/co/jboard1/dto/ArticleDTO.java
@@ -0,0 +1,106 @@
+package kr.co.jboard1.dto;
+
+public class ArticleDTO {
+
+	private int no;
+	private int parent;
+	private String comment;
+	private String cate;
+	private String title;
+	private String content;
+	private int file;
+	private int hit;
+	private String writer;
+	private String regip;
+	private String rdate;
+	
+	//추가필드
+	private String nick;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public void setNo(String no) {
+		this.no= Integer.parseInt(no);
+	}
+	public int getParent() {
+		return parent;
+	}
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+	
+	//오버로드 숫자로 문자로 들어오는 parent를 변환해서 int로 저장.
+	public void setParent(String parent) {
+		this.parent = Integer.parseInt(parent);
+	}
+	public String getCate() {
+		return cate;
+	}
+	public void setCate(String cate) {
+		this.cate = cate;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getFile() {
+		return file;
+	}
+	public void setFile(int file) {
+		this.file = file;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getRegip() {
+		return regip;
+	}
+	public void setRegip(String regip) {
+		this.regip = regip;
+	}
+	public String getRdate() {
+		return rdate.substring(2,10);
+	}
+	
+	public String getFullRdate() {
+		return rdate;
+	}
+	public void setRdate(String rdate) {
+		this.rdate = rdate;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+}
