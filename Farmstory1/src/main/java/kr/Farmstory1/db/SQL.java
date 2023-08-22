cc758eda6fd6cf3e1c94e0cd02b450612d35db15
bllor <duki0105@gmail.com>
2023-08-22 17:02:01 +0900

    장보기 완료

--- Farmstory1/src/main/java/kr/Farmstory1/db/SQL.java
+++ Farmstory1/src/main/java/kr/Farmstory1/db/SQL.java
@@ -33,16 +33,14 @@ public class SQL {
 	
 	public final static String INSERT_COMMENT = "INSERT INTO `Article` SET "
 												+ "`parent`=?, "
-												+ "`cate`=?, "
 												+ "`content`=?,"
 												+ "`writer`=?,"
 												+ "`regip`=?,"
 												+ "`rdate`=NOW()";
 	
 	public final static String SELECT_LATESTS = "SELECT `no`, `title`, `rdate` FROM `Article` "
-												+ "WHERE `cate`=?  and `parent`=0 "
-												+ "order by `no` desc limit ?";
-	
+												+ "WHERE `parent`=0 AND `cate`=? "
+												+ "Order BY `no` DESC LIMIT ?";
 	
 	public final static String SELECT_ARTICLE = "SELECT * FROM `Article` WHERE `no`=?";
 	public final static String SELECT_ARTICLES = "SELECT "
@@ -73,6 +71,25 @@ public class SQL {
 	public final static String DELETE_ARTICLE = "DELETE FROM `Article` WHERE `no`=? OR `parent`=?";
 	public final static String DELETE_COMMENT = "DELETE FROM `Article` WHERE `no`=?";
 	
+	
+	// Product
+	public final static String INSERT_PRODUCT = "INSERT INTO `Product` SET "
+												+ "`type`=?,"
+												+ "`pName`=?,"
+												+ "`price`=?,"
+												+ "`delivery`=?,"
+												+ "`stock`=?,"
+												+ "`thumb1`=?,"
+												+ "`thumb2`=?,"
+												+ "`thumb3`=?,"
+												+ "`seller`=?,"
+												+ "`etc`=?,"
+												+ "`rdate`=NOW()";
+	
+	public final static String SELECT_PRODUCTS_ALL = "SELECT * FROM `Product` WHERE `stock` > 0 LIMIT ?, 10";
+	public final static String SELECT_PRODUCTS_TYPE = "SELECT * FROM `Product` WHERE `stock` > 0 AND `type`=? LIMIT ?, 10";
+	public final static String SELECT_COUNT_PRODUCTS_ALL = "SELECT COUNT(*) FROM `Product` WHERE `stock` > 0";
+	public final static String SELECT_COUNT_PRODUCTS_TYPE = "SELECT COUNT(*) FROM `Product` WHERE `stock` > 0 AND `type`=?";
 	
 	
 	
